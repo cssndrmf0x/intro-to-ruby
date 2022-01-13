dd89e9022b9ee05872cebbdbc21d852eac71c8f2
Cassie Fox <cssndrmfox@gmail.com>
2022-01-13 04:28:44 +0000

    Fixing exercise 5 in the_basics

--- the_basics/exercise5.rb
+++ the_basics/exercise5.rb
@@ -3,9 +3,9 @@
 # Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
 
 fact_array = [5, 6, 7, 8]
-fact_val = 1
 
 fact_array.each do |num|
+  fact_val = 1
   num.downto(1) {|val| fact_val *= val}
   puts "The factorial of #{num} is #{fact_val}"
   fact_val = 1
